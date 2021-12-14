28d62a5f7b6d9dc1b4efe51a0b48f06f65490d33
Giovani Henriques <giovanifvhenriques@gmail.com>
2021-12-14 10:37:41 -0300

    Initial commit

---
+++ src/main/java/com/eventmanager/demo/resource/Resource.java
@@ -0,0 +1,20 @@
+package com.eventmanager.demo.resource;
+
+import java.util.Date;
+import java.util.List;
+
+public class Resource {
+    public String Title;
+
+    public String Description;
+
+    public String Link;
+
+    public Date CreatedDate;
+
+    public Date UpdatedDate;
+
+    public List<String> Keywords;
+
+    
+}
