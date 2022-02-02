ab3e0e0550c1fb8e0b412954704edd689d6e3e07
Giovani Henriques <giovanifvhenriques@gmail.com>
2022-02-02 19:25:48 -0300

    pagination in repositories

--- src/main/java/com/eventmanager/demo/collection/ClassRepository.java
+++ src/main/java/com/eventmanager/demo/collection/ClassRepository.java
@@ -1,6 +1,7 @@
 package com.eventmanager.demo.collection;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface ClassRepository extends CrudRepository<ClassCollection, Integer> {
+public interface ClassRepository extends PagingAndSortingRepository<ClassCollection, Integer> {
 
 }
--- src/main/java/com/eventmanager/demo/collection/EventRepository.java
+++ src/main/java/com/eventmanager/demo/collection/EventRepository.java
@@ -1,13 +1,15 @@
 package com.eventmanager.demo.collection;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.Date;
 import java.util.List;
 
-public interface EventRepository extends CrudRepository<EventCollection, Integer> {
+public interface EventRepository extends PagingAndSortingRepository<EventCollection, Integer> {
     @Query("SELECT m FROM EventCollection m WHERE (:startDate is null or m.startDate >= :startDate) and (:endDate is null or m.endDate <= :endDate)")
-    List<EventCollection> findBetweenDate(Date startDate, Date endDate);
+    List<EventCollection> findBetweenDate(Date startDate, Date endDate, PageRequest pageRequest);
     @Query("SELECT COUNT(m.id) FROM EventCollection m WHERE (:startDate is null or m.startDate >= :startDate) and (:endDate is null or m.endDate <= :endDate)")
     long countBetweenDate(Date startDate, Date endDate);
 }
